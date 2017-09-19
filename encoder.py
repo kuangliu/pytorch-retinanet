a2c27386458f6189e52fa7aab0d7eb14d419d99a
kuangliu <kuang.liu@hotmail.com>
2017-09-19 10:52:00 +0800

    Fix encoder bug

--- encoder.py
+++ encoder.py
@@ -42,15 +42,14 @@ class DataEncoder:
         '''
         num_fms = len(self.anchor_areas)
         fm_sizes = [(input_size/pow(2.,i+3)).ceil() for i in range(num_fms)]  # p3 -> p7 feature map sizes
-        # TODO: make sure computed fm_sizes is the same as feature_map sizes
 
         boxes = []
         for i in range(num_fms):
             fm_size = fm_sizes[i]
-            grid_size = (input_size/fm_size).floor()
+            grid_h, grid_w = (input_size/fm_size).floor()
             fm_h, fm_w = int(fm_size[0]), int(fm_size[1])
             xy = meshgrid(fm_w,fm_h) + 0.5  # [fm_h*fm_w,2]
-            xy = (xy*grid_size).view(fm_h,fm_w,1,2).expand(fm_h,fm_w,9,2)
+            xy = (xy*torch.Tensor([grid_w,grid_h])).view(fm_h,fm_w,1,2).expand(fm_h,fm_w,9,2)
             wh = self.anchor_wh[i].view(1,1,9,2).expand(fm_h,fm_w,9,2)
             box = torch.cat([xy,wh], 3)  # [x,y,w,h]
             boxes.append(box.view(-1,4))
