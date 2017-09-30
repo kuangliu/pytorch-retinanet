e629faed7609517eb0e1c81284a616de4add38ad
kuangliu <kuang.liu@hotmail.com>
2017-09-30 18:47:31 +0800

    Minor fix

--- loss.py
+++ loss.py
@@ -32,10 +32,8 @@ class FocalLoss(nn.Module):
         t = Variable(t).cuda()  # [N,20]
 
         p = x.sigmoid()
-        pt = p.clone()
-        pt[t==0] = 1 - p[t==0]   # pt = p if t>0 else 1-p
-
-        w = alpha*t + (1-alpha)*(1-t)
+        pt = p*t + (1-p)*(1-t)         # pt = p if t > 0 else 1-p
+        w = alpha*t + (1-alpha)*(1-t)  # w = alpha if t > 0 else 1-alpha
         w = w * (1-pt).pow(gamma)
         return F.binary_cross_entropy_with_logits(x, t, w, size_average=False)
 
