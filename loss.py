fda946129b74573c8254c0829ca4a5bff3511460
kuangliu <kuang.liu@hotmail.com>
2017-10-17 14:24:22 +0800

    Add focal loss alternative

--- loss.py
+++ loss.py
@@ -37,6 +37,29 @@ class FocalLoss(nn.Module):
         w = w * (1-pt).pow(gamma)
         return F.binary_cross_entropy_with_logits(x, t, w, size_average=False)
 
+    def focal_loss_alt(self, x, y):
+        '''Focal loss alternative.
+
+        Args:
+          x: (tensor) sized [N,D].
+          y: (tensor) sized [N,].
+
+        Return:
+          (tensor) focal loss.
+        '''
+        alpha = 0.25
+
+        t = one_hot_embedding(y.data.cpu(), 1+self.num_classes)
+        t = t[:,1:]
+        t = Variable(t).cuda()
+
+        xt = x*(2*t-1)  # xt = x if t > 0 else -x
+        pt = (2*xt+1).sigmoid()
+
+        w = alpha*t + (1-alpha)*(1-t)
+        loss = -w*pt.log() / 2
+        return loss.sum()
+
     def forward(self, loc_preds, loc_targets, cls_preds, cls_targets):
         '''Compute loss between (loc_preds, loc_targets) and (cls_preds, cls_targets).
 
@@ -67,7 +90,7 @@ class FocalLoss(nn.Module):
         pos_neg = cls_targets > -1  # exclude ignored anchors
         mask = pos_neg.unsqueeze(2).expand_as(cls_preds)
         masked_cls_preds = cls_preds[mask].view(-1,self.num_classes)
-        cls_loss = self.focal_loss(masked_cls_preds, cls_targets[pos_neg])
+        cls_loss = self.focal_loss_alt(masked_cls_preds, cls_targets[pos_neg])
 
         print('loc_loss: %.3f | cls_loss: %.3f' % (loc_loss.data[0]/num_pos, cls_loss.data[0]/num_pos), end=' | ')
         loss = (loc_loss+cls_loss)/num_pos
